a4dc89d24b8414dfe6dfba07ea81aa046c6aad32
Jake Lokkesmoe <jlokkesmoe@icloud.com>
2017-12-02 22:47:00 -0600

    Simplified app component

--- src/App.js
+++ src/App.js
@@ -35,30 +35,22 @@ const socialLinks = [
   }
 ];
 
-class App extends Component {
-  state = {
-    socialLinks
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <img className="App-picture" src={picture} alt="" />
-        <h1 className="App-title">Developer. Designer. Problem-Solver.</h1>
-        <p>
-          Hi, my name is Jake Lokkesmoe. I am a software developer and designer.
-          I am at my best when diving head first into new challenges. I have a
-          hardwired desire to be better, a strong ambition to make a difference
-          in the world, and an instinct for good design.
-        </p>
-        <p>
-          {this.state.socialLinks.map(link => (
-            <SocialIcon key={link.name} {...link} />
-          ))}
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <img className="App-picture" src={picture} alt="" />
+    <h1 className="App-title">Developer. Designer. Problem-Solver.</h1>
+    <p>
+      Hi, my name is Jake Lokkesmoe. I am a software developer and designer.
+      I am at my best when diving head first into new challenges. I have a
+      hardwired desire to be better, a strong ambition to make a difference
+      in the world, and an instinct for good design.
+    </p>
+    <p>
+      {socialLinks.map(link => (
+        <SocialIcon key={link.name} {...link} />
+      ))}
+    </p>
+  </div>
+);
 
 export default App;
