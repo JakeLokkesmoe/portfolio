41bec44a52eea6e9f8dd2899955184cc5f42f965
Jake Lokkesmoe <jlokkesmoe@icloud.com>
2019-07-10 17:23:50 -0500

    Hide most of app

--- src/App.js
+++ src/App.js
@@ -1,47 +1,6 @@
 import React from "react";
-import SocialIcon from "./components/SocialIcon/SocialIcon";
 import "./App.css";
-import picture from "./resources/jake_240.jpg";
 
-const socialLinks = [
-  {
-    href: "https://twitter.com/JacobLokkesmoe",
-    name: "Twitter",
-    icon: "twitter"
-  },
-  {
-    href: "https://www.linkedin.com/in/jakelokkesmoe",
-    name: "LinkedIn",
-    icon: "linkedin"
-  },
-  {
-    href: "https://www.github.com/JakeLokkesmoe",
-    name: "GitHub",
-    icon: "git"
-  },
-  {
-    href: "mailto:jake@jakelokkesmoe.com",
-    name: "Mail",
-    icon: "envelope-o"
-  }
-];
-
-const App = () => (
-  <div className="App">
-    <img className="App-picture" src={picture} alt="" />
-    <h1 className="App-title">Developer. Designer. Problem-Solver.</h1>
-    <p>
-      Hi, my name is Jake Lokkesmoe. I am a software developer and designer.
-      I am at my best when diving head first into new challenges. I have a
-      hardwired desire to be better, a strong ambition to make a difference
-      in the world, and an instinct for good design.
-    </p>
-    <p>
-      {socialLinks.map(link => (
-        <SocialIcon key={link.name} {...link} />
-      ))}
-    </p>
-  </div>
-);
+const App = () => <div className="App">Hi.</div>;
 
 export default App;
